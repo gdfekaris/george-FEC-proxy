e29bf4041859f56bc82c9cbe04da5158cee83b77
George gd Fekaris <gdfekaris@gmail.com>
2019-01-20 16:07:21 -0800

    mistake

--- proxy/server/server.js
+++ proxy/server/server.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const path = require('path');
-const morgan = require('morgan');
 const cors = require('cors');
-
-const app = express();
-const port = 3000; /* process.env.PORT || 3000; */
-
+const proxy = require('express-http-proxy');
+const app = require('express')();
+const port = process.env.PORT || 3010;
 app.use(cors());
-app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../public')));
+
+// app.use('/users', proxy('http://127.0.0.1:3028/users'));
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
